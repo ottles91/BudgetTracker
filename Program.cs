902f507fe5061d5e9059c0bc4c5b194454db0cb7
Cameron Ottley <cameron.ottley@gmail.com>
2026-03-03 18:07:42 -0700

    Added skeletal driver code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,58 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var budgetService = new BudgetService();
+bool running = true;
+
+while (running)
+{
+    Console.WriteLine("\n1. Add Transaction");
+    Console.WriteLine("2. View All");
+    Console.WriteLine("3. Show Summary");
+    Console.WriteLine("4. Exit");
+
+    Console.Write("Choose an option: ");
+    var input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "1":
+            AddTransactionFlow(budgetService);
+            break;
+        case "2":
+            ViewAllFlow(budgetService);
+            break;
+        case "3":
+            ShowSummaryFlow(budgetService);
+            break;
+        case "4":
+            running = false;
+            break;
+    }
+}
+
+static void AddTransactionFlow(BudgetService service)
+{
+    // TODO:
+    // - Parse decimal
+    // - Parse enum safely
+    // - Handle invalid input
+}
+
+static void ViewAllFlow(BudgetService service)
+{
+    foreach (var transaction in service.GetAllTransactions())
+    {
+        Console.WriteLine(
+            $"{transaction.Date:d} | {transaction.Category} | {transaction.Amount:C} | {transaction.Description}"
+        );
+    }
+}
+
+static void ShowSummaryFlow(BudgetService service)
+{
+    // TODO:
+    // - Print total spent
+    // - Print totals by category
+    // - Print largest expense
+}
